ace1e5357a977fb39a8c1361a59a2c3162447385
FloKnapp <office@florianknapp.de>
2025-05-03 19:10:33 +0200

    Remove vendor autoload

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,6 @@
 use NixPHP\Database\Core\Database;
 use function NixPHP\config;
 
-include_once __DIR__ . '/vendor/autoload.php';
-
 $this->container->set('database', function() {
 
     $config = config('database');
